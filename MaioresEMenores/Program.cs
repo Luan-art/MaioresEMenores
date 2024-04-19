23c0912d2e877225a6edd73380582ece48ae7689
Luan-art <luanlf2003@gmail.com>
2024-04-19 15:37:59 -0300

    Add project files.

---
+++ MaioresEMenores/Program.cs
@@ -0,0 +1,25 @@
+﻿int idade = 0, pessoas = 0, maiores = 0, menores = 0;
+
+while (pessoas < 10)
+{
+    do
+    {
+    Console.WriteLine("Digite sua idade");
+    idade = int.Parse(Console.ReadLine());
+
+    } while (idade < 0);
+
+    if (idade < 18)
+    {
+        menores++;
+    }
+    else
+    {
+        maiores++;
+    }
+
+    pessoas++;
+}
+
+Console.WriteLine($"Temos ao todos {menores} menores de idade");
+Console.WriteLine($"Temos ao todos {maiores} menores de idade");
